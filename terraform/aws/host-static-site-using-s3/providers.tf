4fbc3eb414deb80079d71b38644349a4ae5b713a
Bhuwan Prasad Upadhyay <bhuwan.upadhyay49@gmail.com>
2023-01-30 18:07:25 -0600

    Host s3 static files using terraform

---
+++ terraform/aws/host-static-site-using-s3/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 0.13.1"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "s3" {}
+}
+
+provider "aws" {
+  region = var.region
+}
+
+provider "aws" {
+  alias  = "main"
+  region = var.region
+}
